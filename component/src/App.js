b665c751922a49f243173f155df20d070991cd4a
Gabriel Belgamo <ggbelgamo@outlook.com>
2023-01-12 16:07:49 -0300

    fix: broken image

--- component/src/App.js
+++ component/src/App.js
@@ -7,7 +7,11 @@ function App({ source }) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src="https://media0.giphy.com/media/eNAsjO55tPbgaor7ma/giphy.gif?cid=6c09b95269911e58d4b418309102d0daa0339097a2756244&rid=giphy.gif&ct=s"
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
